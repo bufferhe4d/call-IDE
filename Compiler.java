1cfbc4365cc1748fffd5bdfad2880ec35c68c8af
mahmudsami <mahmudsami97@gmail.com>
2017-03-19 22:02:51 +0200

    Update Compiler.java

--- Compiler.java
+++ Compiler.java
@@ -5,7 +5,7 @@ import  javax.tools.*;
 
 /**
  * This class is a compiler which compile file with file path
- * @author Abdullah Talayhan
+ * @author Abdullah Talayhan, editted by Mahmud Sami Aydin
  * @version 1.00, 19/03/2017
  */
 public class Compiler {
@@ -16,6 +16,11 @@ public class Compiler {
 		
 		compiler =  ToolProvider.getSystemJavaCompiler();
 	}
+	/**
+	 * This method compile a file
+	 * @param f file will be compiled
+	 * @return java compilers output
+	 */
 	
 	public int compile(File f) {
 		
