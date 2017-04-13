b54f428d5bb7046b42c16fa4d04be7da36113de1
Bahadır Tülüce <eminbahadir98@gmail.com>
2017-04-13 15:04:13 +0300

    Arrangements, optimization and new features
    Open folder feature implemented.
    New template feature implemented.
    Change preferences feature partly optimized.
    Source folders, packages and images arranged.
    Packages are renamed to follow the conventions.

---
+++ Call-IDE/src/filebrowser/FileNavigator.java
@@ -0,0 +1,47 @@
+package filebrowser;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.tree.*;
+import java.io.*;
+import java.awt.event.*;
+
+public class FileNavigator extends JTree implements TreeSelectionListener
+{
+    
+    //properties
+    File lastSelectedFile;
+    FileOpener opener;
+    
+    public FileNavigator( String root )
+    { 
+        super( new DefaultTreeModel(new FileNode( new PathedFile( root ))) );
+        setRootVisible(false); 
+        addTreeSelectionListener( this );
+        addMouseListener( new treeMouseListener() );
+    }
+    
+    public FileNavigator( String root, FileOpener opener) {
+        this( root);
+        this.opener = opener;
+    }
+    
+    public void valueChanged(TreeSelectionEvent e)
+    {  
+        lastSelectedFile = ((FileNode) (((JTree)(e .getSource())).getLastSelectedPathComponent( ))).file;
+    }
+    
+    private class treeMouseListener extends MouseAdapter
+    {
+        @Override
+        public void mouseClicked( MouseEvent e)
+        {
+            if( e.getButton() == 1 && e.getClickCount() == 2 && lastSelectedFile.isFile()  )
+            {
+                System.out.println((((FileNode) (((JTree)(e .getSource())).getLastSelectedPathComponent())).file));
+                opener.openFile((((FileNode) (((JTree)(e .getSource())).getLastSelectedPathComponent())).file));
+            }
+        }  
+    }
+    
+}
---
+++ Call-IDE/src/fileoperations/PreferencesConfigurer.java
@@ -0,0 +1,47 @@
+package fileoperations;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+/**
+ * A class to load the preferences of the user
+ * @author Emin Bahadir Tuluce
+ * @version 1.0
+ */
+
+public class PreferencesConfigurer {
+    
+    public static String FILE_NAME = "preferences.dat";
+    
+    /** Saves the given preferences object to the given folder path. */
+    public static void save( String userPath, Preferences preferences) {
+        try{
+            FileOutputStream fileOut = new FileOutputStream( userPath + FILE_NAME); // Initialize the file stream.
+            ObjectOutputStream objOut = new ObjectOutputStream( fileOut); // Initialize the object stream.
+            objOut.writeObject( preferences); // Write the object data.
+            objOut.close(); // Close the object stream.
+            fileOut.close(); // Close the file stream.
+        } catch( IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /** Loads the preferences object that is located on the given folder path. */
+    public static Preferences load( String userPath) {
+        Preferences preferences = null;
+        try {
+            FileInputStream fileIn = new FileInputStream( userPath + FILE_NAME); // Initialize the file stream.
+            ObjectInputStream objIn = new ObjectInputStream( fileIn); // Initialize the object stream.
+            preferences = (Preferences) objIn.readObject(); // Read the object data.
+            objIn.close(); // Close the object stream.
+            fileIn.close(); // Close the file stream.
+      } catch( IOException | ClassNotFoundException e) {
+          e.printStackTrace();
+      }
+      return preferences;
+    }
+    
+}
