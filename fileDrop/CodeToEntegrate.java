05c2687db5f64de27c46934d8f5fb88b4cf13460
Furkan Bıyık <BrainDucker@users.noreply.github.com>
2017-04-26 14:38:45 +0300

    Add files via upload

---
+++ fileDrop/CodeToEntegrate.java
@@ -0,0 +1,11 @@
+new FileDrop( /*component*/, new FileDrop.Listener()
+   {
+      @Override
+      public void filesDropped( java.io.File[] files )
+      {   
+//      for( int i = 0; i < files.length; i++ )
+//      {   
+//         //open files
+//      }  
+      }
+   } );
