2b785bfa45d700249cb508dac3e219a52b5bf63c
mahmudsami <mahmudsami97@gmail.com>
2017-04-28 01:45:13 +0300

    Add files via upload

--- parser/ClassNode.java
+++ parser/ClassNode.java
@@ -9,6 +9,7 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -24,26 +25,26 @@ import javax.swing.tree.DefaultMutableTreeNode;
  */
 public class ClassNode extends DefaultMutableTreeNode {
     
-    ArrayList<Node> innerClasses;
-    Method[]        methods;
+    
+    String  nodeName;
     
     ClassNode( File classFile ) throws ParseException, IOException
     {
         super(classFile.getName());
         CompilationUnit  compUnit;
         
+        nodeName = classFile.getName();
         compUnit = JavaParser.parse(classFile); 
-        methods = compUnit.getClass().getDeclaredMethods();
-        
         
         new ConsturctorVisitor().visit(compUnit, null);
-        new MethodVisitor().visit( compUnit, null);
+        new MethodVisitor().visit( compUnit, null);        
+        new InnerClassVisitor().visit(compUnit, null);
     }
 
 
   
     
-    private class MethodVisitor extends VoidVisitorAdapter<Void> 
+    class MethodVisitor extends VoidVisitorAdapter<Void> 
     {
         @Override
         public void visit(MethodDeclaration n, Void arg) {
@@ -51,7 +52,7 @@ public class ClassNode extends DefaultMutableTreeNode {
              this method will be called for all methods in this 
              CompilationUnit, including inner class methods */
             add( new MethodNode(n));
-            System.out.println(n.getName());
+            //System.out.println(n.getName());
             super.visit(n, arg);
         }
     }
@@ -64,7 +65,21 @@ public class ClassNode extends DefaultMutableTreeNode {
              this method will be called for all methods in this 
              CompilationUnit, including inner class methods */
             add( new ConstructorNode(n));
-            System.out.println(n.getName());
+            //System.out.println(n.getName());
+            super.visit(n, arg);
+        }
+    }
+    
+    private class InnerClassVisitor extends VoidVisitorAdapter<Void> 
+    {
+        @Override
+        public void visit( ClassOrInterfaceDeclaration n, Void arg) {
+            /* here you can access the attributes of the method.
+             this method will be called for all methods in this 
+             CompilationUnit, including inner class methods */
+            System.out.println(n.getNameAsString());
+            if( !nodeName.equals(n.getNameAsString()+ ".java"))
+                add( new InnerNode(n));
             super.visit(n, arg);
         }
     }
---
+++ parser/InnerNode.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import java.util.ArrayList;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ *
+ * @author mahmudsami
+ */
+public class InnerNode extends DefaultMutableTreeNode {
+
+    ArrayList<MethodDeclaration> methods;
+    
+    public InnerNode(ClassOrInterfaceDeclaration n) 
+    {
+        super(n.getName());
+        methods = new ArrayList<MethodDeclaration>(n.getMethods());
+        
+        for(MethodDeclaration method : methods)
+        {
+            add(new MethodNode(method));
+        }
+        
+    }
+    
+}
