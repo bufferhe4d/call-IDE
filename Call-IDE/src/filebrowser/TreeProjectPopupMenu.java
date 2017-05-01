a0a46d4103cd3ecf5acf87bb95442a66ee18a834
mahmudsami <mahmudsami97@gmail.com>
2017-05-01 19:30:05 +0300

    Update TreeProjectPopupMenu.java

--- Call-IDE/src/filebrowser/TreeProjectPopupMenu.java
+++ Call-IDE/src/filebrowser/TreeProjectPopupMenu.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package filebrowser;
 
 import java.awt.event.ActionEvent;
 import javax.swing.JMenuItem;
 
 /**
- *
- * @author mahmudsami
+ * This class represent a pop up menu for project root
+ * @author Mahmud Sami Aydin
  */
 public class TreeProjectPopupMenu extends TreeDirectoryPopupMenu{
     
