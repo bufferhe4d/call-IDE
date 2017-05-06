2ff25cbf1f0ac4d1cd8f0c213c85fc97662809f1
Abdullah Talayhan <abdullahttj@gmail.com>
2017-05-06 21:25:09 +0300

    Recursive Method TODO
    added the recusrive method functionality, it is going to implemented by Mahmud Sami Aydın

--- RealTimeWatcherExample/RealTimeFolderWatcher.java
+++ RealTimeWatcherExample/RealTimeFolderWatcher.java
@@ -11,13 +11,21 @@ package casualtest;
  */
 import com.sun.nio.file.SensitivityWatchEventModifier;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardWatchEventKinds;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,16 +39,24 @@ public class RealTimeFolderWatcher extends Thread {
         
         pathToWatch = dirToWatch.toPath();
         folderWatcher = pathToWatch.getFileSystem().newWatchService();
-        Kind[] events = { StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY};
-        pathToWatch.register(folderWatcher, events, SensitivityWatchEventModifier.HIGH);
+        
+        Kind[] events = { ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY};
+        registerAll(pathToWatch, events);
+        //pathToWatch.register(folderWatcher, events, SensitivityWatchEventModifier.HIGH);
 
         }
         catch (Exception e) {
             e.printStackTrace();
         }
   }
-
-
+  
+  //Path start, root folder to be watched
+  private void registerAll(Path start, Kind[] events) throws IOException {
+      // walk all folders here recursively call the below method for each folder(not file!) the variable dir is a Path Object!!!
+        
+      // call this method for each path: dir.register(folderWatcher, events, SensitivityWatchEventModifier.HIGH);
+    }
+  
   public void run() {
       
         // thread loop
@@ -57,15 +73,17 @@ public class RealTimeFolderWatcher extends Thread {
             for (WatchEvent<?> event : allEvents) {
                 
               // watch each event kind and do the action you want
-              if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE ) {
-                System.out.println("Create Event:: " + event.context().toString());
+              // IMPORTANT: the below method gives the absolute path to the changed file as String (not the file name itself!!!)
+              // Method for getting absolute path:  pathToWatch.resolve((Path) event.context())  
+              if (event.kind() == ENTRY_CREATE ) {
+                System.out.println("Create Event:: " + event.context().toString() + "assumed path:" + pathToWatch.resolve((Path) event.context()));
               }
-              else if(event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
-                  System.out.println("Modify Event: " + event.context().toString());
+              else if(event.kind() == ENTRY_MODIFY) {
+                  System.out.println("Modify Event: " + event.context().toString() + "assumed path:" + pathToWatch.resolve((Path) event.context()));
               }
-              else if(event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+              else if(event.kind() == ENTRY_DELETE) {
 
-                  System.out.println("Delete Event: " + event.context().toString());
+                  System.out.println("Delete Event: " + event.context().toString() + "assumed path:" + pathToWatch.resolve((Path) event.context()));
               }
 
             }
